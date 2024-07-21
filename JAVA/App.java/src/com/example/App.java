6a718fe875989a897209ff4f952f5b75cc2a3e0f
Dipesh Raj Joshi <96607354+Dipeshjoshi26@users.noreply.github.com>
2024-07-21 20:32:05 +0545

    Add files via upload

---
+++ JAVA/App.java/src/com/example/App.java
@@ -0,0 +1,10 @@
+// src/com/example/App.java
+package com.example;
+
+import com.example.utils.helper;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(helper.greet("World"));
+    }
+}
---
+++ JAVA/App.java/src/com/example/utils/helper.java
@@ -0,0 +1,9 @@
+package com.example.utils;
+
+public class helper {
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+    
+}
+
